a41807041dca5cb54b36f0e54e09d0cb0dccbf17
Juan Pablo Sanchez <juanpablo@yconsultores.com>
2024-11-10 12:10:48 -0500

    implementation of swagger

--- src/main/java/com/api/rappi_u/presentation/controllers/DeliveryController.java
+++ src/main/java/com/api/rappi_u/presentation/controllers/DeliveryController.java
@@ -4,9 +4,12 @@ import com.api.rappi_u.application.usecases.DeliveryService;
 import com.api.rappi_u.domain.entities.Delivery;
 import com.api.rappi_u.domain.entities.Order;
 import com.api.rappi_u.infrastructure.dto.LoginDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,27 +17,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/delivery")
+@Tag(name = "Delivery", description = "Delivery controller")
 public class DeliveryController {
 
     @Autowired
     private DeliveryService deliveryService;
 
 
+    @Operation(summary = "Obtener repartidores", description = "Retorna a todos los repartidores")
     @GetMapping
     public List<Delivery> getAllDeliverys() {
         return deliveryService.getAllDeliveries();
     }
 
+    @Schema()
+    @Operation(summary = "Obtener ordenes de repartidor", description = "Retorna todas las ordenes que tiene un repartidos")
     @GetMapping("/orders/{id}")
-    public List<Order> getDeliveryOrders(@PathVariable("id") Long id){
+    public List<Order> getDeliveryOrders(@PathVariable("id") @Parameter(description = "Id del repartidor") Long id) {
         return deliveryService.getDeliveryOrders(id);
     }
 
+    @Operation(summary = "Crear repartidor", description = "Crea un repartidor")
+
     @PostMapping("/register")
-    public Delivery register(@Valid @RequestBody Delivery user) {
+    public Delivery register(@Valid @RequestBody @Parameter(description = "Valores del repartidor") Delivery user) {
         return deliveryService.register(user);
     }
 
+    @Operation(summary = "Logearse como repartidor", description = "Retorna la información del repartidor")
     @PostMapping("/login")
     public ResponseEntity<?> logIn(@Valid @RequestBody LoginDto login) {
         return deliveryService.logIn(login.getEmail(), login.getPassword());
